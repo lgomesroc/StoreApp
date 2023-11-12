11acd1959480318619b41ea85a0b357d6bca5da7
Luciano Gomes Rocha <]'lgomesroc@hotmail.com>
2023-11-12 14:28:44 -0300

    Primeiro programa de API com Testes Unitários

---
+++ StoreApp/Controllers/SimpleProductContrioller.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class SimpleProductController : ApiControllerAttribute 
+    {
+        List<Products> products = new List<Products>();
+
+        public SimpleProductController() { }
+
+        public SimpleProductController(List<Products> products)
+        {
+            this.products = products;
+        }
+
+        public IEnumerable<Products> GetAllProducts()
+        {
+            return products;
+        }
+
+        public async Task<IEnumerable<Products>> GetProductsAsync()
+        {
+            return await Task.FromResult(GetAllProducts());
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(products);
+        }
+
+        private IHttpActionResult Ok(List<Products> products)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IHttpActionResult NotFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IHttpActionResult> GetProductAsync(int id)
+        {
+            return await Task.FromResult(GetProduct(id));
+        }
+
+    }
+}
---
+++ StoreApp/Models/Products.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StoreApp.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price {  get; set; }
+    }
+}
